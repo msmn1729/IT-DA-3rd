f242f2168e9602a6202d9a93cca7527bff1e5993
SGKwon95 <ssunder7@naver.com>
2020-09-08 00:51:26 +0900

    HW 1600 권순규

--- study/Ace/Week1/BOJ_1600_권순규.py
+++ study/Ace/Week1/BOJ_1600_권순규.py
@@ -1,55 +1,49 @@
 from collections import deque
 
-dx, dy = [1,0, -1, 0], [0, 1, 0, -1]
-ndx, ndy = [-2,-2,-1,-1,2,2,1,1 ], [1,-1,2,-2,1,-1,2,-2]
+dx, dy = [1, -1, 0, 0], [0, 0, -1, 1]
+ndx, ndy = [-2, -2, -1, -1, 2, 2, 1, 1], [1, -1, 2, -2, 1, -1, 2, -2]
 
+# 예제 입력 -------------------------------------
 K = int(input())
-W, H = map(int,input().split())
+W, H = map(int, input().split())
 Map = []
-visited1 = [[False for _ in range(W)] for _ in range(H)]
-visited2 = [[False for _ in range(W)] for _ in range(H)]
-destination = [H-1, W-1]
 for i in range(H):
-    tmp = list(map(int,input().split()))
+    tmp = list(map(int, input().split()))
     Map.append(tmp)
+# -----------------------------------------------
+
+visited = [[[False]*(K+1) for _ in range(W)] for _ in range(H)]
+destination = [H-1, W-1]
+
 
 def bfs():
     answer = -1
     q = deque([[0, 0, 0, 0]])
-    visited1[0][0] = True
-    visited2[0][0] = True
+    visited[0][0][0] = True
     while q:
-        y, x, n, d = q.popleft()
+        y, x, n, d = q.popleft()  # y, x, 말로 이동한 횟수, 동작 횟수
         if [y, x] == destination:
             answer = d
             break
-        
-        if n < K:
+
+        if n < K:  # 말로 방문할 수 있으면 말로 방문하기
             for i in range(8):
                 ny = y + ndy[i]
                 nx = x + ndx[i]
-                if nx < 0 or ny < 0 or nx >= W or ny >= H or Map[ny][nx] == 1 or visited1[ny][nx]:
+                if nx < 0 or ny < 0 or nx >= W or ny >= H or Map[ny][nx] == 1 or visited[ny][nx][n+1]:
                     continue
-                visited1[ny][nx] = True
+                visited[ny][nx][n+1] = True
                 q.append([ny, nx, n+1, d+1])
-                
-            for i in range(4):
-                ny = y + dy[i]
-                nx = x + dx[i]
-                if nx < 0 or ny < 0 or nx >= W or ny >= H or Map[ny][nx] == 1 or visited2[ny][nx]:
-                    continue
-                visited2[ny][nx] = True
-                q.append([ny, nx, n, d+1])
 
-        else:
-            for i in range(4):
-                ny = y + dy[i]
-                nx = x + dx[i]
-                if nx < 0 or ny < 0 or nx >= W or ny >= H or Map[ny][nx] == 1 or visited1[ny][nx]:
-                    continue
-                visited1[ny][nx] = True
-                q.append([ny, nx, n, d+1])
-    
+        for i in range(4):
+            ny = y + dy[i]
+            nx = x + dx[i]
+            if nx < 0 or ny < 0 or nx >= W or ny >= H or Map[ny][nx] == 1 or visited[ny][nx][n]:
+                continue
+            visited[ny][nx][n] = True
+            q.append([ny, nx, n, d+1])
+
     return answer
+
 
 print(bfs())
