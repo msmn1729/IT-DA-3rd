8b164ba37640d3981c5249aa092ad1d5ffd06759
MJ-coding <zmffheldk@gmail.com>
2020-09-17 19:31:43 +0900

    Create HW 2234 김민정 (수정)

--- study/JAVA1/Week2/BOJ_2234_김민정.java
+++ study/JAVA1/Week2/BOJ_2234_김민정.java
@@ -4,61 +4,147 @@ import java.io.*;
 
 public class BOJ_2234_김민정 {
 	
-	 	static int[] x = {-1,1,0,0};
-	    static int[] y = {0,0,-1,1};
-	    static int a;
-	    static int b;
-	    static int[][] map;
-	    static boolean[][] visited;
-	    static int count;
-	    static int tmp_square;
-	    static ArrayList<Integer> square = new ArrayList<Integer>();
-	 
-	    public static void main(String[] args) throws Exception{
-	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	        
-	        String[] k = br.readLine().split(" ");
-	        a = Integer.parseInt(k[0]);
-	        b = Integer.parseInt(k[1]);
-	        
-	        map = new int[a][b];
-	        visited = new boolean[a][b];
-	        
-	        for(int i=0; i<a; i++) {
-	            String[] tmp = br.readLine().split(" ");
-	            for(int j=0; j<b; j++) {
-	                map[i][j] = Integer.parseInt(tmp[j]);
-	            }
-	        }
-	        
-	        for(int i=0; i<a; i++) {
-	            for(int j=0; j<b; j++) {
-	                if(map[i][j] == 1 && !visited[i][j]) {
-	                    tmp_square = 1;
-	                    dfs(i,j);
-	                    count++;
-	                    square.add(tmp_square);
-	                }
-	            }
-	        }
-	        
-	        System.out.println(count);
-	        try {
-	            System.out.println(Collections.max(square));
-	        } catch (Exception e) {
-	            System.out.println(0);
-	        }
-	        
-	    }
-	    static void dfs(int k1, int k2) {
-	        visited[k1][k2] = true;
-	        
-	        for(int i=0; i<4; i++) {
-	            if(k1+x[i] >= 0 && k1+x[i] < a && k2+y[i] >= 0 && k2+y[i] < b &&
-	                    map[k1+x[i]][k2+y[i]] == 1 && !visited[k1+x[i]][k2+y[i]]) {
-	                tmp_square++;
-	                dfs(k1+x[i],k2+y[i]);
-	            }
-	        }
-	    }
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+//		sc = new Scanner(src);
+		int M = sc.nextInt();
+		int N = sc.nextInt();
+
+		int[][] map = new int[N][M];
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				map[i][j] = sc.nextInt();
+			}
+		}
+
+		bfs(N, M, map);
+		System.out.println(count);
+		System.out.println(ans);
+		System.out.println(summary);
+	}
+
+	// 1. bfs로 방이 몇개 있는지 부터 구할거임 + Max 방까지 구할 예정
+	static void bfs(int N, int M, int[][] map2) {
+		Queue<Node> q = new LinkedList<>();
+		List<Integer> list = new ArrayList<Integer>();
+		boolean visit[][] = new boolean[N][M];
+
+		int map3[][] = new int[N][M];
+		for (int idx = 0; idx < N; idx++) {
+			for (int jdx = 0; jdx < M; jdx++) {
+				map3[idx][jdx] = map2[idx][jdx];
+			}
+		}
+		int map4[][] = new int[N][M];
+
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				if (!visit[i][j]) {
+
+					visit[i][j] = true;
+					q.add(new Node(i, j, 1));
+					int max = 1;
+					boolean visited[][] = new boolean[N][M];
+					visited[i][j] = true;
+					while (!q.isEmpty()) {
+						Node n = q.poll();
+						int arr[] = new int[4];
+						String s = Integer.toBinaryString(map2[n.y][n.x]);
+						int len = 0;
+						for (int ii = s.length() - 1; ii >= 0; ii--) {
+							arr[len] = s.charAt(ii) - '0';
+							len++;
+							if (len == 0)
+								break;
+						}
+						for (int k = 0; k < arr.length; k++) {
+							if (arr[k] == 0) {
+								int ny = Y[k] + n.y;
+								int nx = X[k] + n.x;
+
+								if (ny < 0 || nx < 0 || ny >= N || nx >= M)
+									continue;
+								if (visit[ny][nx])
+									continue;
+								q.add(new Node(ny, nx, n.cnt + 1));
+								visit[ny][nx] = true;
+								visited[ny][nx] = true;
+								max++;
+							}
+						}
+					}
+					count++;
+					list.add(max);
+					if (ans < max) {
+						ans = max;
+					}
+
+					for (int idx = 0; idx < N; idx++) {
+						for (int jdx = 0; jdx < M; jdx++) {
+							if (visited[idx][jdx]) {
+								map2[idx][jdx] = max;
+								map4[idx][jdx] = count;
+							}
+						}
+					}
+
+				}
+			}
+		}
+		int sum = 0;
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				int arr[] = new int[4];
+				String s = Integer.toBinaryString(map3[i][j]);
+				int len = 0;
+				for (int ii = s.length() - 1; ii >= 0; ii--) {
+					arr[len] = s.charAt(ii) - '0';
+					len++;
+					if (len == 0)
+						break;
+				}
+				for (int k = 0; k < arr.length; k++) {
+					if (arr[k] == 1) {
+						int ny = Y[k] + i;
+						int nx = X[k] + j;
+
+						if (ny < 0 || nx < 0 || ny >= N || nx >= M)
+							continue;
+						if(map4[ny][nx]!=map4[i][j])
+							sum = map2[ny][nx] + map2[i][j];
+					}
+				}
+				if (sum > summary)
+					summary = sum;
+			}
+		}
+	}
+
+	static int summary;
+	static int count;
+	static int ans;
+	static int Y[] = { 0, -1, 0, 1 };
+	static int X[] = { -1, 0, 1, 0 };
+
+	static class Node {
+		int y, x, cnt;
+
+		public Node(int y, int x, int cnt) {
+			super();
+			this.y = y;
+			this.x = x;
+			this.cnt = cnt;
+		}
+
+		@Override
+		public String toString() {
+			return "Node [y=" + y + ", x=" + x + ", cnt=" + cnt + "]";
+		}
+
+	}
+
+	private static String src = "7 4\r\n" + "11 6 11 6 3 10 6\r\n" + "7 9 6 13 5 15 5\r\n" + "1 10 12 7 13 7 5\r\n"
+			+ "13 11 10 8 10 12 13";
 }
