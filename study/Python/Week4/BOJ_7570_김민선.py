9d8613b24dca7c9b551bf9317f48d182cb7c5692
KIMMINSUN12 <meeeeensun@gmail.com>
2020-10-29 14:20:27 +0900

    Craete HW 7570 김민선

--- study/Python/Week4/BOJ_7570_김민선.py
+++ study/Python/Week4/BOJ_7570_김민선.py
@@ -3,7 +3,6 @@ line = list(map(int,input().split()))
 tmp = 0; count = 0
 #맨 앞에는 1이 아니면 1로 하는 게 맞고
 #맨 뒤는 현재 맨 뒤 숫자보다 1 더 큰 숫자가 오는게 맞음
-#시간 초과
 for i in range(n):
     for j in range(n):
         if line[0] != 1 :
