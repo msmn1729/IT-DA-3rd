72423890913dca9801b75af0f8bf84938f6e8038
cheun0327 <48849608+cheun0327@users.noreply.github.com>
2020-11-04 23:52:17 +0900

    Create HW 5052 김채은

---
+++ study/C++2/Week5/BOJ_5052_김채은.cpp
@@ -0,0 +1,53 @@
+//testcase는 통과 그러나 틀림
+#include<iostream>
+#include<string>
+#include<algorithm>
+
+using namespace std;
+int testCase;
+int s[51]={-1,};
+string arr[51][10001]={"n",}; //[testcase횟수][번호 개수]
+int ans[51]={0,};
+
+void Input(){
+	int S;
+	cin>>testCase;
+	for(int i=0; i<testCase; i++){
+		cin>>S;
+		s[i]=S;
+		for(int j=0; j<s[i]; j++){
+			cin>>arr[i][j];
+			if(sizeof(arr[i][j])/sizeof(string)>10){
+				cout<<"10101010";
+				return;
+			}
+		}
+	}
+}
+
+void Solution(){
+	for(int i=0; i<testCase; i++){
+		sort(arr[i],arr[i]+s[i]);
+		for(int j=0; j<s[i]-1; j++){
+			if(string::npos!=arr[i][j+1].find(arr[i][j])){
+				ans[i]=-1;
+				break;
+			}
+		}
+//		if (ans[i] != -1) //not include
+//			ans[i] = 1;
+	}
+	for(int i=0; i<testCase; i++){
+		if(ans[i]==-1)
+			cout<<"NO"<<endl;
+		else
+			cout<<"YES"<<endl;
+	}
+}
+
+int main(){
+	Input();
+	Solution();
+	return 0;
+}
+
