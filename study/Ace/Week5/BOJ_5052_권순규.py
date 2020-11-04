93795c571a793ad1f0944801dd0057b5beb7a16a
SGKwon95 <59722123+SGKwon95@users.noreply.github.com>
2020-11-04 14:35:00 +0900

    Update BOJ_5052_권순규.py

--- study/Ace/Week5/BOJ_5052_권순규.py
+++ study/Ace/Week5/BOJ_5052_권순규.py
@@ -1,29 +1,24 @@
-def compare(short,long):
-    N = len(short)
-    return not short == long[:N]
+for _ in range(int(input())):
+    n = int(input())
 
-if __name__ == "__main__":
-    for _ in range(int(input())):
-        n = int(input())
-        
-        if n == 1:
-            print('YES')
-            continue
-        
-        lst = []
-        
-        for _ in range(n):
-            lst.append(input())
+    if n == 1:
+        print('YES')
+        continue
+
+    lst = []
+
+    for _ in range(n):
+        lst.append(input())
+
+    lst.sort()
     
-        lst.sort()
-        flag = True
-        for i in range(len(lst)-1):
-            if not compare(lst[i],lst[i+1]):
-                flag = False
-                break
-        
-        if flag:
-            print('YES')
-        else:
-            print('NO')
-        
+    flag = True
+    for i in range(len(lst)-1):
+        if lst[i] == lst[i+1][:len(lst[i])]:
+            flag = False
+            break
+
+    if flag:
+        print('YES')
+    else:
+        print('NO')
