8e6a386733f116f27005e62812c03f80da7fd788
whatsbirddd <59131645+whatsbirddd@users.noreply.github.com>
2020-11-05 10:16:06 +0900

    Create HW 5052 박새란

---
+++ study/Python/BOJ_5052_박새란.py
@@ -0,0 +1,30 @@
+"""
+번호의 처음 부분에 다른번호가 포함되면 안됨
+case: combination, both 
+"""
+import sys
+t = int(input())
+phone=[]
+def func(phone):
+    for i in range(1,n):
+        for j in range(i):
+            if phone[j].startswith(phone[i]):
+                return 0
+            elif phone[i].startswith(phone[j]):
+                return 0
+            else :
+                return 1
+
+if __name__ == "__main__":
+    for i in range(t):
+        n = int(input())
+        for _ in range(n):
+            phone.append(sys.stdin.readline().strip())
+        print(phone)
+        if func(phone):
+            print("YES")
+        else:
+            print("NO")
+                  
+        
+        
