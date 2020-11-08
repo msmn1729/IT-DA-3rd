15b8fb3d13d4f51b26ad6d6bea9a2f956a2742b0
SGKwon95 <59722123+SGKwon95@users.noreply.github.com>
2020-11-09 00:42:07 +0900

    Update BOJ_9935_권순규.py

--- study/Ace/Week6/BOJ_9935_권순규.py
+++ study/Ace/Week6/BOJ_9935_권순규.py
@@ -7,7 +7,7 @@ for ch in text:
     answer.append(ch)
     if bombstr[-1] == answer[-1] and len(answer) >= bl:
         flag = True
-        for j in range(-1,-bl-1,-1): # 파이썬만 쓸 수 있는 사기 스킬
+        for j in range(-2,-bl-1,-1): # 파이썬만 쓸 수 있는 사기 스킬
             if answer[j] != bombstr[j]:
                 flag = False
                 break
