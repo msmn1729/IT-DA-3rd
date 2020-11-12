4a0c47fee394a19d36b6da3971587bf87bfe6855
PARKNAYEON <parkna96@gmail.com>
2020-11-12 16:10:49 +0900

    Create HW 14675, 1747, 5525 박나연

---
+++ study/C++2/Week6/BOJ_14675_박나연.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main() {
+	int N, q;
+	pair<int, int> tree;
+
+	cin >> N;
+	for (int i = 1; i < N; i++) {
+		cin >> tree.first;
+		cin >> tree.second;
+	}
+}
