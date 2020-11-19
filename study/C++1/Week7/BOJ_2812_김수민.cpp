d5d6343d6b3b22608c8e9dc9cf8148020936b567
김완태(GOO - KIM WAN TAE) <wtkim@koreanair.com>
2020-11-19 18:15:18 +0900

    Create HW 2812 김수민

---
+++ study/C++1/Week7/BOJ_2812_김수민.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+int N, K;
+vector <int> v;
+
+int main()
+
+{
+	string num;
+	cin >> N >> K;
+	cin >> num;
+	int cnt = K;
+	for (int i = 0; i < N; i++) {
+		while ((!v.empty())&&(cnt>0)&&(v.back() < num[i] - '0')) { //끝에 숫자가 넣을 숫자보다 작으면 큰 숫자 만날때 까지 빼준다
+			v.pop_back();
+			cnt--;
+		}
+		v.push_back(num[i] - '0');
+	}
+	for (int i = 0; i < N - K; i++) {
+		cout << v[i];
+	}
+ 
+}
