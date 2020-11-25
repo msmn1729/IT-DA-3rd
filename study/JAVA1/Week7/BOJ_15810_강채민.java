f46026f77e7f01463bc1fe248bac550c7ba96b4e
Kangchaemin <43837994+Kangchaemin@users.noreply.github.com>
2020-11-25 20:36:17 +0900

    Update HW 15810 강채민

--- study/JAVA1/Week7/BOJ_15810_강채민.java
+++ study/JAVA1/Week7/BOJ_15810_강채민.java
@@ -1,26 +1,42 @@
 import java.util.*;
 
-public class 15810_풍선 공장_ ㅊㅁ {
+public class 15810_ㅊㅁ {
 
 	public static void main(String[] args) {
 		
 		Scanner input = new Scanner(System.in);
 		
 		/**
-		 * 생각했던 접근 방식
+		 * 팀원들의 코드의 도움을 받아 푼 문제입니다.
 		 */
 		
 		int N = input.nextInt(); // 스태프 수
 		int M = input.nextInt(); // 풍선 개수
 		
-		int count = 0;
-		int time = 0;
+		int Minute [] = new int[N];
 		
-		while(count < M) {
+		long left = 1l;
+		long right = 100000000000l;
+		
+		for(int n = 0; n < N; n++)
+			Minute[n] = input.nextInt();
+		
+		while(left <= right) {
 			
-			// 
-			time++;
+			long answer = 0;
+			long mid = (left + right) / 2;
+			
+			for(int i = 0; i < N; i++)
+				answer = answer + (mid / Minute[i]);
+			
+			if(answer >= M)
+				right = mid - 1;
+			
+			else
+				left = mid + 1;
 		}
+		
+		System.out.println(left);
 		
 	}
 
