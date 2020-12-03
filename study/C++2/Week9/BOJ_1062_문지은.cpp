e0bede9b80611db7706e9077df6fbb8ac0de1409
moon jieun <55692557+s2moon98@users.noreply.github.com>
2020-12-03 18:33:43 +0900

    Create HW 1062 문지은

---
+++ study/C++2/Week9/BOJ_1062_문지은.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+int main()
+{
+	int n, k;
+	cin >> n >> k;
+
+	vector <string> s;
+
+	for (int i = 0; i < n; i++)
+	{
+		string str;
+		cin >> str;
+		s.push_back(str);
+	}
+
+	int arr[50] = { 0, };
+
+	
+}
