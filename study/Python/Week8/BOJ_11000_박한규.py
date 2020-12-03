d5c42e494d2d52e195e3bda404a5a478c4a2826e
hanqpark <official03x@gmail.com>
2020-12-03 18:21:30 +0900

    Update BOJ 박한규

--- study/Python/Week8/BOJ_11000_박한규.py
+++ study/Python/Week8/BOJ_11000_박한규.py
@@ -1,3 +1,4 @@
+import heapq
 import sys
 read = sys.stdin.readline
 
@@ -11,11 +12,11 @@ q.sort(key=lambda x: x[1])
 
 for i in range(n):
     if not tt:
-        tt.append(q[i])
+        heapq.heappush(tt, q[i])
         continue
     if tt[0][1] > q[i][0]:
-        tt.append(q[i])
+        heapq.heappush(tt, q[i])
     else:
-        tt.pop(0)
-        tt.append(q[i])
+        heapq.heappop(tt)
+        heapq.heappush(tt, q[i])
 print(len(tt))
--- study/Python/Week8/BOJ_7795_박한규.py
+++ study/Python/Week8/BOJ_7795_박한규.py
@@ -3,14 +3,14 @@ read = sys.stdin.readline
 
 for _ in range(int(read())):
     n, m = map(int, read().split())
-    a = sorted(list(map(int, read().split())))
+    a = list(map(int, read().split()))
     b = sorted(list(map(int, read().split())))
-    cnt = 0
+    cnt = 0     # a > b 갯수 일일이 카운트 해줄 거임
     for i in a:
-        if i > b[-1]:
+        if i > b[-1]:       # a 요소가 b중 가장 큰놈보다 클 경우는 빠르게 처리
             cnt += len(b)
             continue
-        if i <= b[0]:
+        if i <= b[0]:       # a 요소가 b중 가장 작은놈보다 작거나 같으면 어차피 없으니 넘기기
             continue
         left = 0
         right = len(b)-1
@@ -18,5 +18,5 @@ for _ in range(int(read())):
             mid = (left+right)//2
             if b[mid] >= i: right = mid
             else: left = mid
-        cnt += left+1
+        cnt += left+1       # list index는 하나 더 작으니까 +1 처리
     print(cnt)
